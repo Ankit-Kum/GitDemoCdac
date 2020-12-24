9a0498ae7f8c5530e2b4e6c5616530589770ab1d
vijayverma31aug <vijayvermakumar@gmail.com>
2020-12-24 09:51:13 +0530

    Amendments are done

--- src/Test1.java
+++ src/Test1.java
@@ -11,9 +11,9 @@ public class Test1 {
 		System.out.println("HI classs");
 		System.out.println("HI classs");
 		System.out.println("This is new line inserted by Tester");
-		int i=23;
-		int j=34;
-		int k=i+j;
+		int i=23;//The value of i is given here
+		int j=34;//The value of j is given here
+		int k=i+j; //This is the result of addition
 		System.out.println("The result is "+ k);
 		
 		
